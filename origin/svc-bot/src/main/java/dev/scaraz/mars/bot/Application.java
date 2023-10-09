23695a38d7f36241d236091452168d3e80912129
Scarlass\Zaky <zakypundiasa@gmail.com>
2023-10-09 19:34:55 +0700

    bot-svc init

---
+++ origin/svc-bot/src/main/java/dev/scaraz/mars/bot/Application.java
@@ -0,0 +1,15 @@
+package dev.scaraz.mars.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
