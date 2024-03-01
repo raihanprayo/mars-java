ce10ce82e8f5ed0d8587c8f81ff95fa108f06750
Scarlass <zakypundiasa@gmail.com>
2024-03-01 07:18:25 +0700

    remove unnecessary module

--- origin/svc-bot/src/main/java/dev/scaraz/mars/bot/config/WebConfiguration.java
+++
@@ -1,8 +0,0 @@
-package dev.scaraz.mars.bot.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfiguration implements WebMvcConfigurer {
-}
